27eb64d17a7eb7b0211171f2c74c7d8eaa8d1ded
bigo <inred9@gmail.com>
2018-11-14 09:12:16 +0800

    coordinator of all right angles polygen

---
+++ java/src/math/Coordinate.java
@@ -0,0 +1,34 @@
+package math;
+
+public class Coordinate {
+	private double x, y;
+	 
+	public Coordinate(double x, double y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+ 
+	public double getX() {
+		return x;
+	}
+ 
+	public void setX(double x) {
+		this.x = x;
+	}
+ 
+	public double getY() {
+		return y;
+	}
+ 
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	@Override
+	public String toString() {
+		return "x = " + x + ", y=" + y;
+	}
+
+
+}
